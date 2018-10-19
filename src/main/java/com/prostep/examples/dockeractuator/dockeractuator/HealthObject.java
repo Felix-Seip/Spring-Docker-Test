d33947402cc5bcee25fb6d1d7a26a0cda2833c2e
Seip, Felix <Felix.Seip@PROSTEP.com>
2018-10-19 17:15:34 +0200

    Initial Commit

---
+++ src/main/java/com/prostep/examples/dockeractuator/dockeractuator/HealthObject.java
@@ -0,0 +1,15 @@
+package com.prostep.examples.dockeractuator.dockeractuator;
+
+public class HealthObject {
+	
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
---
+++ src/test/java/com/prostep/examples/dockeractuator/dockeractuator/DockerActuatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prostep.examples.dockeractuator.dockeractuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerActuatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
